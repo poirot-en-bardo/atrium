05303d6c2951be566bd0d826aadf63f4c1415916
poirot-en-bardo <balica.eliza@gmail.com>
2026-03-23 13:26:33 +0200

    added SEO elements

---
+++ src/data/siteMeta.ts
@@ -0,0 +1,13 @@
+export const siteMeta = {
+  name: 'Galeria Cutare',
+  language: 'ro',
+  locale: 'ro_RO',
+  defaultDescription:
+    'Galeria Cutare este o galerie de arta contemporana din Campulung-Muscel. Exploreaza expozitii, artisti si informatii de contact.',
+  city: 'Campulung-Muscel',
+  region: 'Arges',
+  countryCode: 'RO',
+  countryName: 'Romania',
+  mapUrl: 'https://maps.google.com/?q=CoArt%2C%20Campulung-Muscel%2C%20Romania',
+  openingHours: ['Mo-Fr 10:00-18:00'],
+} as const;
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from 'astro';
+import { getAllArtists } from '../utils/artists';
+import { getAllExhibitions } from '../utils/exhibitions';
+
+const staticPages = [
+  '',
+  'artisti/',
+  'contact/',
+  'current/',
+  'expozitii/',
+  'locatii/',
+  'past/',
+  'politica-confidentialitate/',
+  'termeni/',
+  'timeline/',
+  'upcoming/',
+];
+
+function toAbsoluteUrl(path: string, site: URL): string {
+  const basePath = import.meta.env.BASE_URL;
+  const normalizedPath = path ? `${basePath}${path}` : basePath;
+  return new URL(normalizedPath, site).toString();
+}
+
+export const GET: APIRoute = ({ site }) => {
+  if (!site) {
+    return new Response('Missing site URL configuration.', { status: 500 });
+  }
+
+  const urls = [
+    ...staticPages.map((path) => toAbsoluteUrl(path, site)),
+    ...getAllArtists().map((artist) => toAbsoluteUrl(`artist/${artist.id}/`, site)),
+    ...getAllExhibitions().map((exhibition) => toAbsoluteUrl(`exhibition/${exhibition.id}/`, site)),
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((url) => `  <url><loc>${url}</loc></url>`).join('\n')}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+};
